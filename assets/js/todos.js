3afae8a81cbefcb3b913458801d2529cf4253e91
Manny Munoz <munoz.jeffrey95@yahoo.com>
2017-08-21 22:05:22 -0400

    App/README

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(300, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// Make new todo after something is entered 
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+
+	}
+});
+
+// Hide button
+$(".fa-plus").click(function() {
+	$("input[type='text']").fadeToggle();
+});
